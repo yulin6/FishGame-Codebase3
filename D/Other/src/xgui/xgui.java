6514ce5110c1ee16255338ad08161654be826d79
Michael Reveliotis <mikeyrev@vdi-linux-055.ccs.neu.edu>
2020-09-20 17:16:49 -0400

    Draw a hexagon

--- D/Other/src/xgui/xgui.java
+++ D/Other/src/xgui/xgui.java
@@ -1,10 +1,21 @@
 package xgui;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Class to provide the functionality of xgui. 
  */
-public class xgui {
+public class xgui extends JFrame {
 
+    public xgui(int size) {
+        //Enough space to fit the hexagon
+        setSize(size * 3, size * 2);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    
+    }
     /**
      * Main method of xgui - draws a hexagon based on argument into the window
      * and closes window & shuts down upon clicking inside it.
@@ -13,6 +24,7 @@ public class xgui {
     public static void main(String[] args) {
         inputCheck(args);
         
+        new xgui(Integer.parseInt(args[0])); 
         // If this point is reached, our input (singular) is valid
         System.out.println("Debug: input value is: " + args[0]);        
         
@@ -41,6 +53,19 @@ public class xgui {
             System.out.println("usage: ./xgui positive-integer");
             System.exit(-3);
         }
+    }
+
+    public void paint(Graphics g) {
+        int w = this.getSize().width - 1;
+        int h = this.getSize().height - 1;
+        Polygon p = new Polygon();
+        p.addPoint(0, h/2);
+        p.addPoint(w/3, h);
+        p.addPoint(2 * (w/3), h);
+        p.addPoint(w, h/2);
+        p.addPoint(2 * (w/3), 0);
+        p.addPoint(w/3, 0);
+        g.drawPolygon(p);
     }
 }
 
