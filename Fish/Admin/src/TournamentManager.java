661b1e06d3a3081946a559618223b9a341d6e582
lin.yu1@northeastern.edu <lin.yu1@husky.neu.edu>
2020-11-18 23:32:49 +0800

    Implemented TournamentManager

--- Fish/Admin/src/TournamentManager.java
+++ Fish/Admin/src/TournamentManager.java
@@ -2,84 +2,144 @@ import player.IPlayerComponent;
 import referee.Referee;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class TournamentManager {
 
+    private List<IPlayerComponent> players;
+    private List<Referee> referees;
+    private TournamentPhase phase;
+    private final int multiple;
 
-//pseudocode
+    private final int MAX_PLAYERS = 4;
+    private final int MIN_PLAYERS = 2;
 
-//    List<IPlayerComponent> playerComponents;
-//    IPlayerComponent leftOutPlayer;
-//    List<GameObserver> observers;
-//    List<Referee> referees;
-//    public TournamentManager(List<IPlayerComponent> playerComponents, List<GameObserver> observers){
-//        this.playerComponents = playerComponents;
-//        this.observers = observers;
-//        this.referees = generateGames();
-//    }
-//
-//
-//
-//    private List<Referee> generateGames(){
-//        //Requirement:
-//        // starts by assigning them to games with the maximal number of participants permitted in ascending order of age.
-//        // Question: how do we get their ages?
-//
-//        int counter = 0;
-//        List<Referee> referees = new ArrayList<>();
-//        List<IPlayerComponent> tmp = new ArrayList<>();
-//        for(IPlayerComponent pc: playerComponents){
-//            tmp.add(pc);
-//            if(tmp.size() == 4){
-//                Referee referee = new Referee(tmp, ..., ...);
-//                referees.add(referee);
-//                tmp.clear();
-//                counter = 0;
-//            } else {
-//                tmp.add(pc);
-//                ++counter;
-//            }
+
+    public TournamentManager(List<IPlayerComponent> players, int multiple) {
+        if (players.size() < MIN_PLAYERS) {
+            throw new IllegalArgumentException("Not enough players to form a tournament.");
+        }
+        this.players = players;
+        informPlayers(InformType.START);
+        this.referees = generateGames();
+        this.multiple = multiple;
+        this.phase = TournamentPhase.RUNNING;
+    }
+
+    private List<Referee> generateGames() {
+        // Requirement:
+        // "starts by assigning them to games with the maximal number of participants permitted in ascending order of age."
+        // Question: how do we get their ages?
+
+        List<Referee> referees = new ArrayList<>();
+        List<IPlayerComponent> oneGamePlayers = new ArrayList<>();
+
+        int playerNumPerGame = findPlayerNumPerGame();
+
+        for (IPlayerComponent pc : players) {
+            oneGamePlayers.add(pc);
+            if (oneGamePlayers.size() == playerNumPerGame) {
+                Referee referee = new Referee(oneGamePlayers, oneGamePlayers.size() * multiple, oneGamePlayers.size() * multiple);
+                referees.add(referee);
+                oneGamePlayers.clear();
+            }
+        }
+
+        if (oneGamePlayers.size() >= MIN_PLAYERS) {
+            int penguinsPerPlayer = oneGamePlayers.size() == 2 ? 4 : 3;
+            Referee referee = new Referee(oneGamePlayers, penguinsPerPlayer * multiple, penguinsPerPlayer * multiple);
+            referees.add(referee);
+        }
+
+        return referees;
+    }
+
+    private int findPlayerNumPerGame() {
+        int playersSize = players.size();
+        int maxSize = MAX_PLAYERS;
+        int remainder = playersSize % maxSize;
+        while (maxSize >= MIN_PLAYERS && maxSize <= MAX_PLAYERS) {
+            if (remainder == MIN_PLAYERS) {
+                --maxSize;
+                remainder = playersSize % maxSize;
+            } else {
+                return maxSize;
+            }
+        }
+
+        throw new IllegalArgumentException("Should not be here");
+    }
+
+
+    public void runTournament() {
+//        if (phase == TournamentPhase.READY) {
+//            phase = TournamentPhase.RUNNING;
 //        }
-//
-//        if (tmp.size() == 1){
-//            leftOutPlayer = tmp.get(0);
-//        } else {
-//            for(IPlayerComponent pc: tmp){
-//                Referee referee = new Referee(pc, ..., ...);
-//                referees.add(referee);
-//            }
-//        }
-//        return referees;
-//    }
-//
-//
-//
-//    public List<Referee> runTournamentRound(){
-//        // if ( 1> remaining players  size <5 ), this is the final game.
-//        // if ( remaining players  size == 1 ), here is the winner
-//
-//        //1. referees run games till the end. (including informing players game is start)
-//        //2. get winning players
-//        //3. add leftOutPlayer if not null
-//        //4. generateGame()
-//    }
-//
-//
-//
-//    public void informPlayers() {
-//        //
-//    }
-//
-//
-//
-//    public void informObservers(){
-//        //mutate fields in GameObserver
-//    }
-//
-//
+        while (phase == TournamentPhase.RUNNING) {
+            runTournamentRound();
+        }
+    }
 
+    public void runTournamentRound() {
+        if (phase == TournamentPhase.RUNNING) {
+            List<IPlayerComponent> winners = runGames();
+            phase = isTournamentEnd(winners) ? TournamentPhase.END : TournamentPhase.RUNNING;
+            players = winners;
+            if (phase == TournamentPhase.RUNNING) {
+                referees = generateGames();
+            } else {
+                informPlayers(InformType.END);
+            }
+        }
+    }
+
+
+    private List<IPlayerComponent> runGames() {
+        List<IPlayerComponent> winners = new ArrayList<>();
+        for (Referee referee : referees) {
+            referee.notifyGameStart();
+            referee.runGame();
+            referee.notifyGameEnd();
+            winners.addAll(referee.getWinners());
+        }
+        return winners;
+    }
+
+    private boolean isTournamentEnd(List<IPlayerComponent> winners) {
+        return players.containsAll(winners) || winners.size() < MIN_PLAYERS;
+    }
+
+    public List<IPlayerComponent> getWinners() {
+        if (phase == TournamentPhase.END) {
+            informPlayers(InformType.END);
+            return players;
+        } else {
+            throw new IllegalArgumentException("Tournament has not end."); //should it throw?
+        }
+    }
+
+    private void informPlayers(InformType type) {
+        if (type == InformType.START) {
+            for (IPlayerComponent player : players) {
+                //remove the players who failed to communicate
+            }
+        } else {
+            for (IPlayerComponent player : players) {
+                //remove the players who failed to communicate
+            }
+        }
+    }
+
+    public enum TournamentPhase {
+        //        READY,
+        RUNNING,
+        END
+    }
+
+    private enum InformType {
+        START,
+        END
+    }
 
 }
 
