07d8df830425189d0fde56abf68c83b1631b2500
hansenj <hansen.j@husky.neu.edu>
2020-11-25 15:33:26 -0500

    game-visualizer.java points to FishFrame, FishPanel, and FishController

--- Fish/Admin/game-visualizer.java
+++ Fish/Admin/game-visualizer.java
@@ -1,1 +1,4 @@
-// see panhandle/9/Other/src/Xgui.java
+// see the following files:
+// - Fish/Common/src/game/view/FishFrame.java
+// - Fish/Common/src/game/view/FishPanel.java
+// - Fish/Common/src/game/controller/FishController.java
